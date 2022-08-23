a0eb6a591c9671681fe1deddbbcf7227e231b3d4
Medric Sonwa <medric49@gmail.com>
2022-08-23 16:45:31 -0400

    update

--- train.py
+++ train.py
@@ -5,6 +5,7 @@ import metaworld_env
 warnings.filterwarnings('ignore', category=DeprecationWarning)
 import os
 os.environ['MKL_SERVICE_FORCE_INTEL'] = '1'
+os.environ['DISABLE_MUJOCO_RENDERING'] = '1'
 import torch
 
 
