4f09a86a1983a29690770c50ecfd026f9e1bc60b
Medric Sonwa <medric49@gmail.com>
2022-08-22 23:02:45 -0400

    Add train script

---
+++ cc/train_reacher_hard.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1       # Request GPU "generic resources"
+#SBATCH --cpus-per-task=8  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
+#SBATCH --mem=32G       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
+#SBATCH --time=10:00:00
+#SBATCH --output=%N-%j.out
+#SBATCH --account=def-eugenium
+#SBATCH --mail-user=medric49@gmail.com
+#SBATCH --mail-type=ALL
+
+module load python
+source venv/bin/activate
+
+python train.py task=reacher_hard exp_id=1 exp_group=reacher_hard
