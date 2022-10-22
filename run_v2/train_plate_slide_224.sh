60d5d2daf4b4aa0ac2578826c50d5d8e9c607b8a
Medric Sonwa <medric49@gmail.com>
2022-10-22 17:51:49 -0400

    update script

--- run_v2/train_plate_slide_224.sh
+++ run_v2/train_plate_slide_224.sh
@@ -1,4 +1,4 @@
-python3 scripts/generate_metaworld_video.py --env plate_slide --episode_len 50 --im-w 224 --im-h 224 --video-dir plate_slide_224 --num-train 1500 --num-valid 150
+# python3 scripts/generate_metaworld_video.py --env plate_slide --episode_len 50 --im-w 224 --im-h 224 --video-dir plate_slide_224 --num-train 1500 --num-valid 150
 
 python3 train_cmc.py task=plate_slide_224 exp_id=1
 
