c550db6620751e9a0f19082a21c467a2b04cf198
Medric Sonwa <medric49@gmail.com>
2022-10-25 19:20:32 -0400

    update config

--- run_v2/train_button_press_224.sh
+++ run_v2/train_button_press_224.sh
@@ -1,5 +1,5 @@
 # python3 scripts/generate_metaworld_video.py --env button_press --episode_len 50 --im-w 224 --im-h 224 --video-dir button_press_224 --num-train 1500 --num-valid 150
 #
-# python3 train_cmc.py task=button_press_224 exp_id=1
+python3 train_cmc.py task=button_press_224 exp_id=1
 
 python3 train_rlv2.py task=button_press_224
